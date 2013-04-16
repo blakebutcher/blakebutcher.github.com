65d747056ea680873887eb00edcbf9dbdc6858b6
Blake Butcher <blake.butcher@gmail.com>
2013-04-17 08:09:36 +1000

    Added watch config for changes to SCSS and JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,18 +14,47 @@ module.exports = function(grunt) {
 			}
 		},
 		compass: {
-			dist: {
+			all: {
 				options: {
 					config: 'config.rb'
 				}
 			}
+		},
+		watch: {
+			styles: {
+				files: 'assets/styles/*.scss',
+				tasks: ['compass'],
+				options: {
+					interrupt: true
+				}
+			},	
+			scripts: {
+				files: 'assets/scripts/*.js',
+				tasks: ['jshint', 'uglify'],
+				options: {
+					interrupt: true
+				}
+			}			
 		}
+		/*compare_size: {
+			files: [ "assets/scripts/scripts.js", "assets/scripts/min/scripts.js" ],
+			options: {
+				compress: {
+					gz: function( contents ) {
+						return gzip.zip( contents, {} ).length;
+					}
+				},
+				cache: "temp/.sizecache.json"
+			}
+		}*/
 	});
 
 	// Load the plugins
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	//grunt.loadNpmTasks('grunt-compare-size');
 
 	// Set task(s)
 	grunt.registerTask('default', ['jshint', 'uglify', 'compass']);
