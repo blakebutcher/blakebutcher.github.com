fb7fb98498ababafb00023eed3e224d0bff2a822
Blake Butcher <blake.butcher@gmail.com>
2013-04-17 22:33:50 +1000

    Updated Uglify config to process any JS files under assets/scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,15 @@ module.exports = function(grunt) {
 				report: 'min'
 			},
 			all: {
-				files: {
-					'assets/scripts/min/scripts.js': ['assets/scripts/scripts.js']
-				}
+				files: [
+					{
+						expand: true, // Enable dynamic expansion.
+						cwd: 'assets/scripts/', // Src matches are relative to this path.
+						src: ['*.js'], // Actual pattern(s) to match.
+						dest: 'assets/scripts/min/', // Destination path prefix.
+						ext: '.js' // Dest filepaths will have this extension.
+					}
+				]
 			}
 		},
 		compass: {
